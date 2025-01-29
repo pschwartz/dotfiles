4cb3660ca0f278061bf6ff1c7990f77d11024bd4
Philip Schwartz <philip@progmad.com>
2025-01-29 14:25:14 -0600

    Remove copied scripts and update paths
    Signed-off-by: Philip Schwartz <philip@progmad.com>

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 stow .
+
+if [ -d "HOME" ]; then
+  for rc in $(ls HOME); do
+    if [ ! -f ${HOME}/.${rc} ]; then
+      echo "Linking ${PWD}/HOME/${rc} to ${HOME}/.${rc} ..."
+      ln -s ${PWD}/HOME/${rc} ${HOME}/.${rc}
+    fi
+  done
+fi
